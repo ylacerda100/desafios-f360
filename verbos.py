0138961f12c8053b0d4101f906710fbdbc16f9a5
Ygor Lacerda <ylacerda100@gmail.com>
2020-12-18 11:30:08 -0300

    Add files via upload

---
+++ verbos.py
@@ -0,0 +1,14 @@
+file = open("textoB.txt", "r")
+
+letras_foo = ["s", "l", "f", "w", "k"]
+
+with file as arquivo:
+    verbos = 0
+    for linha in arquivo:
+        palavras = linha.split()
+        for palavra in palavras:
+            # encontrando verbos kinglon
+            if len(palavra) >= 8 and palavra[-1:] in letras_foo:
+                verbos += 1
+print(verbos)
+file.close()
